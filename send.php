956a5ef842a5aa04c231ff83f8f31b6391e1e40b
Aldo Nuñez <aldo.andres.nunez@gmail.com>
2021-09-20 21:27:51 -0300

    con formulario completo

--- send.php
+++ send.php
@@ -1,26 +1,56 @@
 <?php
-error_reporting(0);
-$nombre = $_POST['nombre'];
-$correo_electronico= $_POST['email'];
-$mensaje=$_POST['mensaje'];
-$header = 'From: ' . $mail . ", de la poblacion ".$poblacion."\r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+/**
+ * @version 1.0
+ */
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . " \r\n";
-$mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "sexo" . $_POST['GrupoOpciones1'] . " \r\n";
-$mensaje .= "aficiones " . $_POST['comentarios'] . " \r\n";
-$mensaje .= "que opinas de nuestra pagina" . $_POST['GrupoOpciones2'] . " \r\n";
-$mensaje .="danos tu opinion".$_POST['opinion'] . " \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
+require("./php/class.phpmailer.php");
+require("./php/class.smtp.php");
 
-$para = 'info@fundacion-despertar.com';
-$asunto = 'Mensaje de webmail';
+// Valores enviados desde el formulario
+if ( !isset($_POST["nombre"]) || !isset($_POST["email"]) || !isset($_POST["mensaje"]) || !isset($_POST["telefono"]) ) {
+    die ("Es necesario completar todos los datos del formulario");
+}
+$nombre = $_POST["nombre"];
+$email = $_POST["email"];
+$mensaje = "Mensaje: " . $_POST["mensaje"] . "\nTelefono: " . $_POST["telefono"] . "\r\n";
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
 
-echo 'mensaje enviado correctamente';
+// Datos de la cuenta de correo utilizada para enviar vía SMTP
+$smtpHost = "c2340106.ferozo.com";  // Dominio alternativo brindado en el email de alta 
+$smtpUsuario = "no-reply@c2340106.ferozo.com";  // Mi cuenta de correo
+$smtpClave = "sXbEHlwDmk";  // Mi contraseña
 
+// Email donde se enviaran los datos cargados en el formulario de contacto
+$emailDestino = "info@fundacion-despertar.com";
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Port = 465; 
+$mail->SMTPSecure = 'ssl';
+$mail->IsHTML(true); 
+$mail->CharSet = "utf-8";
+
+
+// VALORES A MODIFICAR //
+$mail->Host = $smtpHost; 
+$mail->Username = $smtpUsuario; 
+$mail->Password = $smtpClave;
+
+$mail->From = $email; // Email desde donde envío el correo.
+$mail->FromName = $nombre;
+$mail->AddAddress($emailDestino); // Esta es la dirección a donde enviamos los datos del formulario
+
+$mail->Subject = "Mensaje desde fundacion-despertar.com"; // Este es el titulo del email.
+$mensajeHtml = nl2br($mensaje);
+$mail->Body = "{$mensajeHtml} <br /><br/>Mensaje generado a través del formulario de contacto"; // Texto del email en formato HTML
+$mail->AltBody = "{$mensaje} \n\n Enviado desde la web"; // Texto sin formato HTML
+// FIN - VALORES A MODIFICAR //
+
+$estadoEnvio = $mail->Send(); 
+if($estadoEnvio){
+    echo "El correo fue enviado correctamente.";
+} else {
+    echo "Ocurrió un error inesperado.";
+}
 ?>
